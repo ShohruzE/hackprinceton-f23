7df7a8d033e9e2154f55ff2e95bfd4596e2afdd8
Shohruz Ernazarov <shohruzernazarov04@gmail.com>
2023-11-11 01:02:33 -0500

    complete product frontend and backend integration

--- server/server.js
+++ server/server.js
@@ -2,17 +2,23 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-const app = express();
+import promptsRouter from './routes/prompt.js';
+
+dotenv.config();
 
 const PORT = process.env.PORT || 3001;
+
+const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+app.use('/api', promptsRouter);
+
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.status(200).json({ message: 'Hello World!' });
 })
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-})
+});
