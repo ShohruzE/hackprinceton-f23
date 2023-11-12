b8e11f6eecddbe763cdbfbd51c137205a5e060a8
Sumayia <sumayia04@gmail.com>
2023-11-11 19:22:25 -0500

    front end changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,26 +2,29 @@ import React from 'react'
 import '../styles/Navbar.css';
 import '../styles/index.css';
 
+import logo from "../../public/logo.png"
+
 import { Link, Outlet } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <>
+    <main>
         <div className="Navbar">
             <nav>
+                <ul className='title'>
+                    <Link to="/"><h2>HoneyDo</h2></Link>
+                    <Link to="/"><img src={logo} /></Link>
+                </ul>
                 <ul>
-                    <Link to="/">Home</Link>
                 </ul>
                 <ul className='right-links'>
                     <Link to="/product">Product</Link>
                     <Link to="/pricing">Pricing</Link>
                 </ul>
             </nav>
-            
         </div>
         <Outlet />
-    </>
-
+    </main>
   )
 }
 
